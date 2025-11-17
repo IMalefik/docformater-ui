e17b63086c1b9586f500f6ce6ed81f3d6ce276b1
Сергей Григоренко <sergejgrigorenko@Sergejs-MacBook-Air.local>
2025-11-17 23:42:59 +0300

    Add simple Yandex Metrika img counter

--- app_ui.py
+++ app_ui.py
@@ -4,32 +4,19 @@ import base64
 import uuid
 import re
 
-# ---------------- Yandex Metrika ----------------
-
-YA_METRICA = """
-<!-- Yandex.Metrika counter -->
-<script type="text/javascript">
-    (function(m,e,t,r,i,k,a){
-        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-        m[i].l=1*new Date();
-        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=105367333', 'ym');
-
-    ym(105367333, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
-</script>
-<noscript><div><img src="https://mc.yandex.ru/watch/105367333" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-<!-- /Yandex.Metrika counter -->
-"""
+st.markdown(
+    """
+    <img src="https://mc.yandex.ru/watch/105367333"
+         style="position:absolute; left:-9999px;" alt="" />
+    """,
+    unsafe_allow_html=True
+)
 
 # ------------------ CONFIG ------------------
 
 BACKEND_URL = "https://docformater.onrender.com/assemble"
 
 st.set_page_config(page_title="DocFormatter", layout="wide")
-
-# вставляем Метрику
-st.markdown(YA_METRICA, unsafe_allow_html=True)
 
 # Шрифт Montserrat
 st.markdown(
